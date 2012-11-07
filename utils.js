367a837f9b80feeeeb17ebdbdbe0ecee7263fb94
Atul Varma <varmaa@gmail.com>
2012-11-07 15:52:04 +0000

    added Utils.getExifInfo().

--- utils.js
+++ utils.js
@@ -13,5 +13,51 @@ var Utils = {
       ctx.drawImage(image, 0, 0, sSize, sSize, 0, 0, size, size);
       callback(canvas.toDataURL());
     };
+  },
+  getExifInfo: function(arrayBuffer) {
+    var oFile = new this.ArrayBufferBinaryFile(arrayBuffer);
+    return EXIF.readFromBinaryFile(oFile);
+  },
+  ArrayBufferBinaryFile: function ArrayBufferBinaryFile(buffer) {
+    var dataView = new DataView(buffer);
+
+    this.getByteAt = function(iOffset) {
+      var result = dataView.getUint8(iOffset);
+      //console.log("getByteAt", iOffset, result);
+      return result;
+    };
+
+    this.getLength = function() {
+      //console.log("getLength", buffer.byteLength);
+      return buffer.byteLength;
+    };
+
+    this.getShortAt = function(iOffset, bBigEndian) {
+      var result = dataView.getUint16(iOffset, !bBigEndian);
+      //console.log("getShortAt", iOffset, bBigEndian);
+      return result;
+    };
+
+    this.getLongAt = function(iOffset, bBigEndian) {
+      var result = dataView.getUint32(iOffset, !bBigEndian);
+      //console.log("getLongAt", iOffset, bBigEndian);
+      return result;
+    };
+
+    this.getSLongAt = function(iOffset, bBigEndian) {
+      var result = dataView.getInt32(iOffset, !bBigEndian);
+      //console.log("getSLongAt", iOffset, bBigEndian);
+      return result;
+    };
+
+    this.getStringAt = function(iOffset, iLength) {
+      var chars = [];
+      for (var i = iOffset; i < iOffset + iLength; i++)
+        chars.push(String.fromCharCode(dataView.getUint8(i)));
+      var result = chars.join('');
+      //var result = dataView.getInt32(iOffset, !bBigEndian);
+      //console.log("getStringAt", iOffset, iLength, result);
+      return result;
+    };
   }
 };
